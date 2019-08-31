e792b69c767a7a8bc01e9a4ee0c139956673dcfb
Louis Quinnell <zioroboco@gmail.com>
2019-08-31 23:41:09 +1000

    Deploy an empty stack

--- src/stack.ts
+++ src/stack.ts
@@ -1,3 +1,20 @@
-import { pretties } from "./functions/fly"
+import * as cdk from "@aws-cdk/core"
 
-console.log(pretties)
+export const STACK_NAME = "fly-my-pretties"
+
+export class Stack extends cdk.Stack {
+  constructor(app: cdk.App, id: string, props: cdk.StackProps) {
+    super(app, id, props)
+  }
+}
+
+const personalAccount: cdk.Environment = {
+  account: "203242799745",
+  region: "ap-southeast-2",
+}
+
+export function synth(env = personalAccount) {
+  const app = new cdk.App()
+  new Stack(app, STACK_NAME, { env })
+  app.synth()
+}
