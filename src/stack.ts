855f32a95244d5d76c448b4c39c1864a3f1a86d3
Louis Quinnell <zioroboco@gmail.com>
2019-09-01 12:07:08 +1000

    Add basic inline lambda and endpoint

--- src/stack.ts
+++ src/stack.ts
@@ -1,10 +1,37 @@
+import * as apiGateway from "@aws-cdk/aws-apigateway"
+import * as lambda from "@aws-cdk/aws-lambda"
 import * as cdk from "@aws-cdk/core"
 
 export const STACK_NAME = "fly-my-pretties"
 
+const logEventFunction = `
+exports.handler = function(event, context, callback) {
+  console.log('Event: ', event)
+  callback(null, event)
+}
+`
+
+type FunctionProps = { code: string }
+
+export class Function extends lambda.Function {
+  constructor(scope: cdk.Construct, id: string, { code }: FunctionProps) {
+    super(scope, id, {
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromInline(code),
+    })
+  }
+}
+
 export class Stack extends cdk.Stack {
   constructor(app: cdk.App, id: string, props: cdk.StackProps) {
     super(app, id, props)
+
+    new apiGateway.LambdaRestApi(this, "Endpoint", {
+      handler: new Function(this, "LogEvent", {
+        code: logEventFunction,
+      }),
+    })
   }
 }
 
